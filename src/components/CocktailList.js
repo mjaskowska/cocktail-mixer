b1a132b1264ffdae6f58d03092f80d9926c0a6b5
Magda <jaskowska.magda@gmail.com>
2021-01-20 17:54:22 +0000

    feat: fetch API

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -2,7 +2,12 @@ import React, {useEffect} from 'react'
 import {API_URL} from '../assets/API'
 
 const CocktailList = () => {
-
+    
+    useEffect(()=>{
+        fetch(API_URL)
+        .then((res)=> res.json())
+        .then(data => console.log(data))
+      }, [])
 
     return (
         <div>
