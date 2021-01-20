a30f3a0c7ad165ae95c653ec4d88d7096c26054b
Magda <jaskowska.magda@gmail.com>
2021-01-20 17:54:03 +0000

    initial commit

---
+++ src/components/CocktailList.js
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react'
+import {API_URL} from '../assets/API'
+
+const CocktailList = () => {
+
+
+    return (
+        <div>
+            <p></p>
+        </div>
+    )
+}
+
+export default CocktailList
