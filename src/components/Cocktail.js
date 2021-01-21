184ccec1591cbcf1a386e37c676e7ec0a0a72c05
Magda <jaskowska.magda@gmail.com>
2021-01-21 15:32:09 +0000

    add card styling

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './styles/Cocktail.css'
 
 const Cocktail = ({item}) => {
-    
+
+   
+
     return (
-        <div className="drink-card">
-            {item.strDrink}
-            <img className="drink-img"src={item.strDrinkThumb} alt={item.strDrink}></img>
+        <div className="drink-card" >
+            <p className="drink-title">{item.strDrink}</p>
+            <img className="drink-img"src={item.strDrinkThumb} dataid={item.idDrink} alt={item.strDrink}></img>
         </div>
     )
 }
--- src/components/Cocktails.js
+++ src/components/Cocktails.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Cocktail from './Cocktail'
 import LoadingIcon from "./LoadingIcon";
+import './styles/Cocktails.css'
 
 const Cocktails = ({ isLoading, items }) => {
+
+const handleClick = () => {
+    console.log('clicked')
+}
+
   return isLoading ? (
     <LoadingIcon />
   ) : (
-      <section>
+      <section className="cocktail-container">
           {items.map((item)=>{
-              return <Cocktail key={item.idDrink} item={item} />
+              return <Cocktail key={item.idDrink} item={item}/>
           })}
       </section>
    
